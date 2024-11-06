82df4c98b73221cdc4aaba67446fea914c841269
Siva Balan <sivabalan.r@innoppl.com>
2024-11-06 15:59:22 +0530

    modified sh codes

--- buildspec_update_lambda_code.sh
+++ buildspec_update_lambda_code.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 echo "Lambda files got changed. Going to update lambda function code"
-aws ssm get-parameters-by-path --path /Test-LAMBDA --region us-west-2 | jq -r '.Parameters | map(.Name+"="+.Value)| join("\n") | sub("/Test-LAMBDA/"; ""; "g")  ' >.envmkdir -p /tmp/lamda/Test
-echo Copying Files from Project to Temp Folder
-rsync -a  Test/ /tmp/lamda/Test/
-mv /tmp/lamda/Test/Test.py /tmp/lamda/Test/lambda_function.py
-cp .env /tmp/lamda/Test/.env
-cd /tmp/lamda/Test/ && zip -rq ../Test.zip .
-aws s3 cp /tmp/lamda/Test.zip s3://your-bucket/lambda_functions/Test-Lambda/Test.zip
+
+# Fetch parameters from SSM and output them to .env
+aws ssm get-parameters-by-path --path /Test-LAMBDA --region us-west-2 | \
+jq -r '.Parameters | map(.Name + "=" + .Value) | join("\n") | sub("/Test-LAMBDA/"; ""; "g")' > .env
+
+# Create necessary directories
+mkdir -p /tmp/lambda/Test
+
+# Copy files to the temporary directory
+echo "Copying files from project to temp folder"
+rsync -a Test/ /tmp/lambda/Test/
+mv /tmp/lambda/Test/Test.py /tmp/lambda/Test/lambda_function.py
+cp .env /tmp/lambda/Test/.env
+
+# Zip the files and upload to S3
+cd /tmp/lambda/Test && zip -rq ../Test.zip .
+aws s3 cp /tmp/lambda/Test.zip s3://your-bucket/lambda_functions/Test-Lambda/Test.zip
+
+# Update Lambda function code
 aws lambda update-function-code --function-name TestLambda --s3-bucket your-bucket --s3-key lambda_functions/Test-Lambda/Test.zip
